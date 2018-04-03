062231245db52618af6a3910b35bdfbe90bb2c0c
Manisha11sep <er.manisha11sep@gmail.com>
2018-04-03 12:03:59 -0700

    added middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('session');
 
+const {createInitalSession} = require './middlewares/session.js'
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret: 'aaabbc',
+    resave: 'false',
+    saveUninitalized: 'true',
+    cookie: {
+        maxAge: 1000*10,
+
+    }
+}));
+
+app.use(createInitalSession);
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
---
+++ server/middlewares/session.js
@@ -0,0 +1,14 @@
+module.exports ={
+
+    createInitalSession: (req,res, next) =>{
+        if(req.session.user)
+        {
+             req.session = {
+                user:{
+                    messages: []
+                }
+            }
+        }    
+        next(); 
+  }
+}
